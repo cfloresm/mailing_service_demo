28fe0f74e2f7dab32bd71c5815dc662075ff17a8
carlos <cfloresm@anzen.com.mx>
2014-01-10 16:28:51 -0600

    Change import package for controller .

--- src/main/java/mx/com/prosa/app/mail/controller/WebAppController.java
+++ src/main/java/mx/com/prosa/app/mail/controller/WebAppController.java
@@ -12,6 +12,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -184,7 +185,6 @@ public class WebAppController {
 		model.addAttribute("signup", new Signup());
 		return "index";
 	}
-
 
 
 }
